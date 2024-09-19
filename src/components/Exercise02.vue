ee6faae35bb41a0e802494d5fd461b4561349bfa
haducluonghnk2023 <haducluongdz@gmail.com>
2024-09-19 14:31:06 +0700

    ad

---
+++ src/components/Exercise02.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <div v-bind:id="elementId" v-bind:class="elementClass">Heading</div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+const elementId = "heading";
+const elementClass = "heading";
+</script>
+
+<style></style>
---
+++ src/components/Exercise10.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+   
+    <!-- Phần tử input cho phép người dùng nhập -->
+    <input
+      type="text"
+      v-model="inputText"
+      @keydown="handleKeydown"
+      placeholder="Nhập văn bản và nhấn Shift + Enter"
+    />
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const inputText = ref("");
+
+const handleKeydown = (event) => {
+  if (event.key === "Enter") {
+    if (event.shiftKey) {
+      alert(inputText.value);
+      inputText.value = "";
+    } else {
+      alert("Bạn cần nhấn Shift + Enter");
+    }
+  }
+};
+</script>
+
+<style scoped>
+input {
+  width: 300px;
+}
+</style>
